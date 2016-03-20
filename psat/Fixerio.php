a8ce2ab49d860898bc9c798be14b8646a8c626ba
php <php@php-Komputer>
2016-03-20 22:51:37 +0100

    Update README.md
    czy dobrze to robiłem to newiem interpretacja  do społecznosci

--- psat/Fixerio.php
+++ psat/Fixerio.php
@@ -14,41 +14,35 @@ class Fixerio {
         $this->apiClient = new \GuzzleHttp\Client();
     }
 
-    public function getCurrencies() { 
+    public function getCurrencies() {
 
         $response = $this->collectDataCurrency();
-        
+
         $this->currencySymbols = $this->getCurrencySymbols($response);
-     return $this->currencySymbols;
-        
+        return $this->currencySymbols;
     }
 
     private function formatResponse($symb1, $symb2) { // wylicza ratio 
-
         $cur1 = $response->rates->$symb1;
         $cur2 = $response->rates->$symb2;
-        $resp=$cur1/$cur2;
+        $resp = $cur1 / $cur2;
         return $resp;
-                
     }
 
     private function getCurrencySymbols($response) { // buduje liste dostepnych walut 
-
         $tmp = $response->rates;
         $tmp = json_decode(json_encode($tmp), true);
         $tmp = array_keys($tmp);
-        $tmpArray=array();
-        for ($i=0;$i <= count($tmp)-1;$i++){
-            $tmpArray[$tmp[$i]] =  $tmp[$i];
+        $tmpArray = array();
+        for ($i = 0; $i <= count($tmp) - 1; $i++) {
+            $tmpArray[$tmp[$i]] = $tmp[$i];
         }
-    return $tmpArray;
-        
-        
+        return $tmpArray;
     }
 
     private function collectDataCurrency() { // pobiera i psprawdza czy nie ma dnych w cache
         $data = date('Y-m-d');
-$expiresAt =  \Carbon\Carbon::now()->addMinutes(4600);
+        $expiresAt = \Carbon\Carbon::now()->addMinutes(4600);
         if (Cache::has($data)) {
             $response = Cache::get($data);
 //            echo("Z pamieci");
@@ -61,7 +55,7 @@ $expiresAt =  \Carbon\Carbon::now()->addMinutes(4600);
         }
 //           $base = $response->base;
 //           $response = $response->rates[$base]=1;  // chciłem dodać walute bazową 
-           
+
         return $response;
     }
 
